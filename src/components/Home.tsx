de614bcddd569d231a50f6622b0c7fb45c92f158
rachelsitoh <rachelsyt99@gmail.com>
2024-02-12 23:37:23 +0800

    add mobile display

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "./NavBar.tsx";
+import NavbarMobile from "./NavBarMobile.tsx";
 const Home: React.FC = () => {
   return (
     <div>
-      <Navbar />
+      <NavbarMobile />
       <h1>Home</h1>
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,43 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/NavBar.css";
 import personLogo from "../assets/img/person_logo.png";
 import fintechLogo from "../assets/img/fintech_logo.png";
 
 function Navbar(): JSX.Element {
+  const [showProfileOptions, setShowProfileOptions] = useState(false);
+
+  const toggleProfileOptions = () => {
+    setShowProfileOptions(!showProfileOptions);
+  };
+
   return (
-    <header className="overallHeader">
-      <div>
-        <img src={fintechLogo} alt="fintech Logo" className="fintechLogo" />
+    <header>
+      <div className="overallHeader">
+        <div>
+          <img src={fintechLogo} alt="fintech Logo" className="fintechLogo" />
+        </div>
+        <div>
+          <Link to="/announcements" className="Link">
+            Announcements
+          </Link>
+        </div>
+        <div>
+          <Link to="/members" className="Link">
+            Members
+          </Link>
+        </div>
+        <div>
+          <Link to="/events" className="Link">
+            Events
+          </Link>
+        </div>
+        <div>
+          <Link to="/tasks" className="Link">
+            Tasks
+          </Link>
+        </div>
+        <div>
+          <Link to="/recruitment" className="Link">
+            Recruitment
+          </Link>
+        </div>
+        <div>
+          <img
+            src={personLogo}
+            alt="Person Logo"
+            className="personLogo"
+            onClick={toggleProfileOptions}
+          />
+        </div>
       </div>
-      <div>
-        <Link to="/announcements" className="Link">
-          Announcements
-        </Link>
-      </div>
-      <div>
-        <Link to="/members" className="Link">
-          Members
-        </Link>
-      </div>
-      <div>
-        <Link to="/events" className="Link">
-          Events
-        </Link>
-      </div>
-      <div>
-        <Link to="/tasks" className="Link">
-          Tasks
-        </Link>
-      </div>
-      <div>
-        <Link to="/recruitment" className="Link">
-          Recruitment
-        </Link>
-      </div>
-      <div>
-        <img src={personLogo} alt="Person Logo" className="personLogo" />
-      </div>
+      {showProfileOptions && (
+        <div className="profileOptions">
+          <div className="profileOption">
+            <p>Profile</p>
+          </div>
+          <div className="signOutOption">
+            <p>Sign Out</p>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
