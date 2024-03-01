6bb6ce0a3b94e3f806ccd64ab67ff795c6a90ade
Andrew Janong <andrewjanong@gmail.com>
2024-03-01 17:01:31 +0800

    Create Login Form

---
+++ src/components/LoginFormComponent.tsx
@@ -0,0 +1,21 @@
+import styles from '../assets/css/LoginFormComponent.module.css';
+import SignInButton from './SignInButton';
+
+const LoginFormComponent = () => {
+
+    return (
+        <div className={styles.login}>
+            <p>Login</p>
+            <input type="text" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+
+            <a href="" className={styles['forgot-password']}>Forgot your password?</a>
+
+            <div className={styles['login-button-container']}>
+                <SignInButton />
+            </div>
+        </div>
+    )
+}
+
+export default LoginFormComponent
---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,12 @@
+import styles from '../assets/css/SignInButton.module.css';
+
+const SignInButton = () => {
+
+    return (
+        <button className={styles['sign-in-button']}>
+            Sign In
+        </button>
+    )
+}
+
+export default SignInButton
