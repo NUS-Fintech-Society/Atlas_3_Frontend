fb5eb2ac0290c049660626a5d55ced406c9799da
brannn99 <bransometan@gmail.com>
2024-03-14 23:50:59 +0800

    Used CSS Modules

--- src/components/events/EventTag.tsx
+++ src/components/events/EventTag.tsx
@@ -1,18 +1,28 @@
-
-import '../../assets/css/events/EventTag.css';
+import styles from "../../assets/css/events/EventTag.module.css";
 
 interface EventTagProps {
-    name: string;
-    datetime: Date;
-    backgroundColor: string;
+  name: string;
+  datetime: Date;
+  backgroundColor: string;
 }
 
-const EventTag: React.FC<EventTagProps> = ({ name, backgroundColor }: EventTagProps) => {
-    return (
-        <div className="event-tag-container" style={{ backgroundColor }}>
-        <span className={backgroundColor == "#0C1747" ? "event-tag-font-white" : "event-tag-font-black"}>{name}</span>
-        </div>
-    );
-}
+const EventTag: React.FC<EventTagProps> = ({
+  name,
+  backgroundColor,
+}: EventTagProps) => {
+  return (
+    <div className={styles["event-tag-container"]} style={{ backgroundColor }}>
+      <span
+        className={
+          backgroundColor == "#0C1747"
+            ? styles["event-tag-font-white"]
+            : styles["event-tag-font-black"]
+        }
+      >
+        {name}
+      </span>
+    </div>
+  );
+};
 
 export default EventTag;
