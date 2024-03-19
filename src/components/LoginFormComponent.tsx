c5c350a945c186603414745642890486b3ede1b8
Andrew Janong <andrewjanong@gmail.com>
2024-03-19 20:11:53 +0800

    add signIn method to signIn button

--- src/components/LoginFormComponent.tsx
+++ src/components/LoginFormComponent.tsx
@@ -1,18 +1,22 @@
+import { useState } from 'react';
+
 import styles from '../assets/css/LoginFormComponent.module.css';
 import SignInButton from './SignInButton';
 
 const LoginFormComponent = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     return (
         <div className={styles.login}>
             <p>Login</p>
-            <input type="text" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
+            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
             <a href="" className={styles['forgot-password']}>Forgot your password?</a>
 
             <div className={styles['login-button-container']}>
-                <SignInButton />
+                <SignInButton email={email} password={password}/>
             </div>
         </div>
     )
--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,9 +1,16 @@
 import styles from '../assets/css/SignInButton.module.css';
 
-const SignInButton = () => {
+import { signIn } from '@/api/authentication';
+
+interface Props {
+    email: string;
+    password: string;
+}
+
+const SignInButton = (props: Props) => {
 
     return (
-        <button className={styles['sign-in-button']}>
+        <button className={styles['sign-in-button']} onClick={() => signIn(props.email, props.password)}>
             Sign In
         </button>
     )
