afd11f833de6391d3073ad3ee0168efdc805feb4
Lu Xinming <xinmingsm11@gmail.com>
2024-06-08 09:45:42 +0800

    import firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
@@ -14,6 +15,9 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
+export const db = getFirestore(app);
+
 if (import.meta.env.DEV) {
   connectAuthEmulator(auth, "http://localhost:9099");
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
 }
